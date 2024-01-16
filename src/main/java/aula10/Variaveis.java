b7184ffae8566646a79ecffea98e0ba62eadc134
CamilaStolte <mila.stolte15@gmail.com>
2024-01-16 10:05:19 -0300

    Aula 10

---
+++ src/main/java/aula10/Variaveis.java
@@ -0,0 +1,16 @@
+package aula10;
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+        int idade = 20;
+        String nome = "Loiane";
+        String nomeDoMeuCachorro = "totó"; 
+
+        idade = 25;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Nome: " + nome);
+        System.out.println("Nome do meu cachorro: " + nomeDoMeuCachorro);
+    }
+}
