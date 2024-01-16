b0557cee1a36e93d2fe84aa619e0d1122061c548
CamilaStolte <mila.stolte15@gmail.com>
2024-01-16 13:09:14 -0300

    Aula13

---
+++ src/main/java/Aula13/OperadoresAritmerticos.java
@@ -0,0 +1,48 @@
+package Aula13;
+
+public class OperadoresAritmerticos {
+
+	public static void main(String[] args) {
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+		
+		resultado = resultado -1;
+		System.out.println(resultado);
+		
+		resultado = resultado * 2;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7;
+		System.out.println(resultado);
+		
+		String primeiroNome = "Esta é ";
+		String segundoNome = "uma String concatenada ";
+		String terceiroNome = primeiroNome + segundoNome;
+		System.out.println(terceiroNome);
+		
+		resultado = resultado + 1;
+		System.out.println(resultado);
+		
+		resultado++;
+		System.out.println(resultado);
+		
+
+		System.out.println(resultado++);
+		//System.out.println(resultado)
+		// resultado + 1
+		//resultado +=1
+
+		System.out.println(++resultado);
+		//resultado +=1
+		//System.out.println(resultado)
+		
+		
+	}
+
+}
