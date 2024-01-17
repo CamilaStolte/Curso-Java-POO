b4e75a316d4402f41a9e9d46d16f5ba9c692e235
CamilaStolte <mila.stolte15@gmail.com>
2024-01-17 15:41:00 -0300

    Aula 16

---
+++ src/main/java/Aula16/loopWhile.java
@@ -0,0 +1,28 @@
+package Aula16;
+
+public class loopWhile {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int max = 10;
+		
+		System.out.println("Contando até " + max);
+		
+		
+		while (i <= max) {
+			System.out.println("valor de i:  " + i);
+			i++ ;
+
+		}
+		System.out.println(i);
+		
+		do {
+			i++;
+			System.out.println("Valor de i: " + i);
+		} while (i < 15);
+		
+		System.out.println(i);
+	}
+
+}
