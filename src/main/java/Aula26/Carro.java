6fbd7283ebf050d255e937efe3ac5450a17c05c3
CamilaStolte <mila.stolte15@gmail.com>
2024-01-23 08:09:38 -0300

    Aula 26

---
+++ src/main/java/Aula26/Carro.java
@@ -0,0 +1,23 @@
+package Aula26;
+
+public class Carro {
+	String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+    void exibirAutonomia() {
+        System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + " km");
+    }
+    
+    double obterAutonomia() {
+    	
+    	System.out.println("Método obter autonomia foi chamado");
+    	
+    	return capCombustivel * consumoCombustivel;
+    }
+   
+}
+
+
